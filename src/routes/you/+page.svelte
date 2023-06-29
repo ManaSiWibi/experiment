fa5e444c7dfb22f548c07bb85ae2e4f516924c5a
Wibi <ardywibisana@protonmail.com>
2023-06-29 22:13:07 +0700

    first setup

---
+++ src/routes/you/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	let m = { x: 0, y: 0 };
+
+	function closeWindow() {
+		close();
+	}
+</script>
+
+<svelte:window on:keydown={closeWindow} />
+
+
+<div class="flex justify-center items-center h-screen">
+    <button class="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600 focus:outline-none" on:click={closeWindow}>
+      Click to Die!
+    </button>
+  </div>
